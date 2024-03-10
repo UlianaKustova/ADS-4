4fff6ef3dd5f1d372bc7158ccabd8caf1a985529
UlianaKustova <91365332+UlianaKustova@users.noreply.github.com>
2024-03-10 19:26:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -26,7 +26,7 @@ int countPairs2(int* arr, int len, int value) {
         if (arr[i] > value) {
             break;
         }
-        if (x < i) {
+        if (x <= i) {
             j = i + 1;
         } else {
             j = x;
@@ -49,30 +49,58 @@ int countPairs2(int* arr, int len, int value) {
 int countPairs3(int* arr, int len, int value) {
     int count = 0, y = 0, min = 0, max = len;
     for (int i = 0; i < (len - 1); i++) {
+        if (arr[i] > value) {
+            break;
+        }
         min = i;
         max = len - 1;
-        while (min <= max) {
+        while (min < max) {
             y = min + ((max - min) / 2);
-            if ((arr[i] + arr[y]) == value) {
-                break;
-            }
             if (min == max) {
                 break;
             }
             if ((arr[i] + arr[y]) > value) {
-                max = y;
-                if (max - min == 1) {
-                    break;
+                for (int n = (y - 1); n >= min; n--) {
+                    max = n + 1;
+                    if (arr[y] != arr[n]) {
+                        break;
+                    }
                 }
-            } else {
-                min = y;
                 if (max - min == 1) {
                     break;
                 }
             }
+            if ((arr[i] + arr[y]) < value) {
+                for (int n = (y + 1); n <= max; n++) {
+                    min = n - 1;
+                    if (arr[y] != arr[n]) {
+                        break;
+                    }
+                }
+                if (max - min == 1) {
+                    break;
+                }
+            }
+            if ((arr[i] + arr[y]) == value) {
+                int t = max;
+                for (int n = (y + 1); n <= t; n++) {
+                    max = n - 1;
+                    if (arr[y] != arr[n]) {
+                        break;
+                    }
+                }
+                t = min;
+                for (int n = (y - 1); n >= t; n--) {
+                    min = n + 1;
+                    if (arr[y] != arr[n]) {
+                        break;
+                    }
+                }
+                break;
+            }
         }
         for (int j = min; j <= max; j++) {
-            if ((arr[i] + arr[j]) == value) {
+            if (((arr[i] + arr[j]) == value) && (i != j)) {
                 count++;
             }
         }
